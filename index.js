491991e705c2905384b054b42ead19f43ad01eb2
sebamomann <github@sebamomann.de>
2021-09-28 21:46:26 +0200

    callbacks

--- index.js
+++ index.js
@@ -16,11 +16,18 @@ bot.command('quit', (ctx) => {
 
 bot.command("remind", (ctx) => {
     ctx.reply(`Yo ${ctx.message.from.username}`)
+    var options = {
+        reply_markup: JSON.stringify({
+            inline_keyboard: [
+                [{ text: 'Some button text 1', callback_data: '1' }],
+                [{ text: 'Some button text 2', callback_data: '2' }],
+                [{ text: 'Some button text 3', callback_data: '3' }]
+            ]
+        })
+    };
 
     ctx.reply('Moin',
-        Markup.keyboard([
-            ['Items', 'Lol']
-        ])
+        options
     )
 })
 
