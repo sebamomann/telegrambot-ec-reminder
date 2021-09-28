4178af89d919714a411a23b17136203300f2ea59
sebamomann <github@sebamomann.de>
2021-09-28 21:47:07 +0200

    .

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ bot.command("remind", (ctx) => {
     var options = {
         reply_markup: JSON.stringify({
             inline_keyboard: [
-                [{ text: 'Some button text 1', callback_data: '1' }],
-                [{ text: 'Some button text 2', callback_data: '2' }],
+                [{ text: 'Some button text 1', callback_data: '1' }, { text: 'Some button text 2', callback_data: '2' }],
                 [{ text: 'Some button text 3', callback_data: '3' }]
             ]
         })
