e04a89e4261ffdc024eca1b8d5d25574164dfa97
sebamomann <github@sebamomann.de>
2021-10-03 22:13:48 +0200

    remove comments

--- index.js
+++ index.js
@@ -9,26 +9,15 @@ import * as reminder from './commands/reminder.js'
 const bot = new Telegraf(process.env.BOT_API_TOKEN);
 
 const localSession = new LocalSession({
-    // Database name/path, where sessions will be located (default: 'sessions.json')
     database: 'session_db.json',
-    // Name of session property object in Telegraf Context (default: 'session')
     property: 'session',
-    // Type of lowdb storage (default: 'storageFileSync')
     storage: LocalSession.storageFileAsync,
-    // Format of storage/database (default: JSON.stringify / JSON.parse)
     format: {
         serialize: (obj) => JSON.stringify(obj, null, 2), // null & 2 for pretty-formatted JSON
         deserialize: (str) => JSON.parse(str),
     },
     // We will use `messages` array in our database to store user messages using exported lowdb instance from LocalSession via Telegraf Context
     state: { messages: [] }
-})
-
-// Wait for database async initialization finished (storageFileAsync or your own asynchronous storage adapter)
-localSession.DB.then(DB => {
-    // Database now initialized, so now you can retrieve anything you want from it
-    console.log('Current LocalSession DB:', DB.value())
-    // console.log(DB.get('sessions').getById('1:1').value())
 })
 
 // Telegraf will use `telegraf-session-local` configured above middleware with overrided `property` name
